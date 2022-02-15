ac469d81c579597ea77b395129bf399c37677514
Andres Collazos <andrescollazosc@gmail.com>
2022-02-14 21:57:25 -0500

    Added new gRPC client in c#

---
+++ GrpcClientACCAMD/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceACCAMD;
+using System;
+
+namespace GrpcClientACCAMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress(" https://localhost:7134");
+            GrpcPersonService.GrpcPersonServiceClient client = new GrpcPersonService.GrpcPersonServiceClient(channel);
+            var result = client.GetPerson(new StatusPersonRequest { Id = 2 });
+
+            Console.WriteLine($"{ result.FirstName } { result.LastName }");
+
+            Console.ReadKey();
+
+        }
+    }
+}
